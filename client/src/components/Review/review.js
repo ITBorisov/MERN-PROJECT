32f0d60b3247a3da7c5f4cb20df83efe7cbe5859
Georgi Borisov <georgi.borisov.it@gmail.com>
2018-07-14 21:32:57 +0300

    refactoring

--- client/src/components/Review/review.js
+++ client/src/components/Review/review.js
@@ -3,29 +3,30 @@ import { getReviewWithReviewer } from '../../actions/reviews';
 import moment from 'moment-js';
 import { connect } from 'react-redux';
 
-const style ={
-    color: "black",
-    height: '80vh',
-    paddingTop: "30px",
-    width: "50%",
-    margin: "0 auto",
+const style = {
+  color: "black",
+  height: '80vh',
+  paddingTop: "30px",
+  width: "50%",
+  margin: "0 auto",
 }
 
 export class ReviewView extends Component {
 
-    componentWillMount() {
-        this.props.dispatch(getReviewWithReviewer(this.props.match.params.id))
-    }
+  componentWillMount() {
+    this.props.dispatch(getReviewWithReviewer(this.props.match.params.id))
+  }
 
-    renderBook = (reviews) => (
-        reviews.review ?
-        <div className="well" style={style}>
+  renderBook = (reviews) => (
+    reviews.review ?
+      <div className="well" style={style}>
         <div className="media">
           <a className="pull-left" href="#">
-            <img className="media-object" src={reviews.review.image} style={{width: 700}} />
+            <img className="media-object" src={reviews.review.image} style={{ width: "50%" }} />
           </a>
           <div className="media-body">
-            <h4 className="media-heading">{reviews.review.name}</h4>
+            <br />
+            <h2 className="media-heading" style={{color: "black"}}>{reviews.review.name}</h2>
             <p className="text-right">{reviews.review.author}</p>
             <p>{reviews.review.review}</p>
             <ul className="list-inline list-unstyled">
@@ -50,26 +51,32 @@ export class ReviewView extends Component {
             </ul>
           </div>
         </div>
+        <div className="form-group">
+          <label for="comment">Comment:</label>
+          <textarea className="form-control" rows="5" id="comment"></textarea>
+        </div>
+        <button>ADD COMMENT</button>
       </div>
-      
-        : null
-    );
-        
-    render() {
-            let reviews = this.props.reviews;
-            console.log(reviews);
-            return (
-                <div>
-                    {this.renderBook(reviews)}
-                </div>
-            )
-    }
+
+      : null
+  );
+
+  render() {
+    let reviews = this.props.reviews;
+    console.log(reviews);
+    return (
+      <div>
+        {this.renderBook(reviews)}
+       
+      </div>
+    )
+  }
 }
-        
+
 function mapStateToProps(state) {
-    return {
-            reviews: state.reviews
-            }
+  return {
+    reviews: state.reviews
+  }
 }
-        
+
 export default connect(mapStateToProps)(ReviewView)
